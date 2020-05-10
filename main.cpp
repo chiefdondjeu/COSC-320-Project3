d554bd26b15690c0a7384e01efffc59c5fad53bd
chiefdondjeu <florentdondjeu@yahoo.com>
2020-05-10 04:03:42 -0400

    removed comments

--- main.cpp
+++ main.cpp
@@ -58,44 +58,5 @@ int main(int argc, char **argv)
 	std::cout << "\nend of program. Existing...\n";
 	std::cout << "========================================\n";
 
-	/*Graph<char> myGraph;
-
-	//pixels on the grids
-	myGraph.addVertex('A',20);
-	myGraph.addVertex('B',30);
-	myGraph.addVertex('C',9);
-	myGraph.addVertex('D',12);
-	myGraph.addVertex('E',1);
-	myGraph.addVertex('F',17);
-	myGraph.addVertex('G',16);
-	myGraph.addVertex('H',9);
-	myGraph.addVertex('I',2);
-	myGraph.addVertex('J',10);
-
-	//pixels's edges
-	myGraph.addEdge('A','B');
-	myGraph.addEdge('A','C');
-	myGraph.addEdge('A','I');
-
-	myGraph.addEdge('B','C');
-	myGraph.addEdge('B','J');
-	myGraph.addEdge('B','E');
-
-	myGraph.addEdge('C','D');
-
-	myGraph.addEdge('D','E');
-	myGraph.addEdge('D','F');
-
-	myGraph.addEdge('E','J');
-	myGraph.addEdge('E','H');
-
-	myGraph.addEdge('F','G');
-	myGraph.addEdge('G','H');
-
-	myGraph.print();
-
-	std::cout << "\n";
-	myGraph.Segment();*/
-
 	return 0;
 }
