f412292bdb6f6790dc82a23f03aa8279f2acc880
chiefdondjeu <florentdondjeu@yahoo.com>
2020-05-10 04:01:28 -0400

    patched up alg and fixed comments

--- Graph.h
+++ Graph.h
@@ -11,7 +11,6 @@
 
 #include<vector>
 #include<map>
-#include<queue> 
 #include<iostream>
 #include<algorithm>
 
@@ -22,7 +21,7 @@ class Graph
 
 		struct Pixel
 		{
-			T data;
+			T data;	//other pixel
 			double weight;	//difference in intensity between two pixels
 
 			Pixel(T d, double w)
@@ -32,25 +31,35 @@ class Graph
 			}
 		};
 
-		std::map<T,std::vector<Pixel> > V; 	//pixels and their edges with other pixels
+		std::map<T,std::vector<Pixel> > V; 	//pixel's edges
 		std::map<T,double> W;	//pixels and their intensity
 
-		//int vertcount, edgecount;
-
-		std::map< double,std::pair<T,T> > pi;
-
-		//returns a positive difference of two number
+		//returns a positive difference of two numbers
 		double difference(double&,double&);
 
-		//help prints content stored in vector
+		//helper function to print content stored in a vector
 		void printVect(std::vector<Pixel>&);
 
+		//helper function to search for an element in a vector
 		bool isPresent(std::vector<T>,T);
 
+		/* Algorithm functions */
+
+		std::map<double,std::pair<T,T> > pi;	//edge's intensity
+
+		//determines if two elements are in different components
+		//return true if element are in different component
+		//returns false if elements are in the same component
 		bool isDisjoint(std::vector<std::vector<T> >,T,T);
 
-		double internal_size(std::vector<std::vector<T> >, T);
+		//controls the degree of difference betwee two components
+		double tau(int);
 
+		//add all the weight of each element in a component
+		//returns the minimun of the two
+		double internal_size(std::vector<std::vector<T> >, T,T);
+
+		//combines the pixel from two components into one 
 		void merge(std::vector<std::vector<T> >&, T,T);
 
 
@@ -60,36 +69,45 @@ class Graph
 
 		~Graph();
 
-		//add a pixel
+		//adds a pixel to the graph
 		void addVertex(T,double);
 
-		//add edge between 2 pixels
+		//adds an undirected edge between 2 pixels
 		void addEdge(T,T);
 
-		//prints all pixels along with their relative information
+		//prints all pixels along with their intensity and their adjacency list, and edge weight
 		void print();
 
-		void Segment();	//TODO: need to figure out return type
+		//segements the graph by putting each pixel in a component and merges pixels
+		//with similar intensity thus creating sub-regions of the graphs
+		void Segment();
 };
 
-/*	DEFINITIONS */
+/*  DEFINITIONS  */
 
 template <class T>
-Graph<T>::Graph()//: vertcount(0), edgecount(0)
+Graph<T>::Graph()
 {}
 
 template <class T>
 Graph<T>::~Graph()
 {}
 
+/*
+	returns a positive difference of two numbers	
+*/
+
 template <class T>
-//returns a positive difference of two number
 double Graph<T>::difference(double& a, double& b)
 {
 	if((a-b) < 0)
 		return -(a-b);
 	else return (a-b);
 }
+
+/*
+	adds a pixel to the graph
+*/
 
 template <class T>
 void Graph<T>::addVertex(T v, double w)
@@ -99,13 +117,15 @@ void Graph<T>::addVertex(T v, double w)
 		std::vector<Pixel> vect;
 		V[v] = vect;
 		W[v] = w;
-
-		//vertcount++;
 	}
 }
 
+/*
+	adds an undirected edge between 2 pixels
+*/
+
 template <class T>
-void Graph<T>::addEdge(T u, T v)	//undirected edge
+void Graph<T>::addEdge(T u, T v)
 {
 	//pixel have already been added and no self edges
 	if(V.find(u) != V.end() && V.find(v) != V.end() && u != v)
@@ -118,13 +138,15 @@ void Graph<T>::addEdge(T u, T v)	//undirected edge
 
 		std::pair<T,T> p = std::make_pair(u,v);
 		pi[difference(iteru->second,iterv->second)] = p;
-
-		//edgecount++;
 	}
 }
 
+/*
+	helper function to print content stored 
+	in a vector
+*/
+
 template <class T>
-//help prints content stored in vector
 void Graph<T>::printVect(std::vector<Pixel>& vect)
 {
 	for(int i = 0; i < vect.size(); i++)
@@ -132,6 +154,11 @@ void Graph<T>::printVect(std::vector<Pixel>& vect)
 		std::cout << vect.at(i).data << "(" << vect.at(i).weight << ") ";
 	}
 }
+
+/*
+	prints all pixels along with their intensity 
+	and their adjacency list, and edge weight
+*/
 
 template <class T>
 void Graph<T>::print()
@@ -150,6 +177,10 @@ void Graph<T>::print()
 	}
 }
 
+/*
+	helper function to search for an element in a vector
+*/
+
 template <class T>
 bool Graph<T>::isPresent(std::vector<T> vect, T x)
 {
@@ -160,6 +191,12 @@ bool Graph<T>::isPresent(std::vector<T> vect, T x)
 	}
 	return false;
 }
+
+/*
+	determines if two elements are in different components
+	return true if element are in different component
+	otherwise false
+*/
 
 template <class T>
 bool Graph<T>::isDisjoint(std::vector<std::vector<T> >S, T x,T y)
@@ -175,11 +212,26 @@ bool Graph<T>::isDisjoint(std::vector<std::vector<T> >S, T x,T y)
 	return false;
 }
 
-//returns the internal weight of a component
+/*
+	controls the degree of difference betwee two components
+*/
+
 template <class T>
-double Graph<T>::internal_size(std::vector<std::vector<T> >S, T x)
+double Graph<T>::tau(int x)
 {
-	int sum = 0;
+	return 50/x;
+}
+
+/*
+	add all the weight of each element in a component
+	returns the minimun of the two
+*/
+
+template <class T>
+double Graph<T>::internal_size(std::vector<std::vector<T> >S, T x, T y)
+{
+	int min, sum1 = 0, sum2 = 0;
+
 	for(int i = 0; i < S.size(); i++)
 	{
 		if(isPresent(S.at(i),x))
@@ -189,13 +241,36 @@ double Graph<T>::internal_size(std::vector<std::vector<T> >S, T x)
 				for(auto l = W.begin(); l != W.end(); l++)
 				{
 					if(l->first == S.at(i).at(j))
-						sum += l->second;
+						sum1 += l->second;
+					sum1+= tau(S.at(i).at(j));
+				}
+			}
+		}
+		if(isPresent(S.at(i),y))
+		{
+			for(int j = 0; j < S.at(i).size(); j++)
+			{
+				for(auto l = W.begin(); l != W.end(); l++)
+				{
+					if(l->first == S.at(i).at(j))
+						sum2 += l->second;
+					sum2+= tau(S.at(i).at(j));
 				}
 			}
 		}
 	}
-	return sum;
+
+	if(sum1 < sum2)
+		min = sum1;
+	else min = sum2;
+
+	return min;
 }
+
+/*
+	combines the pixel from two 
+	components into one 
+*/
 
 template <class T>
 void Graph<T>::merge(std::vector<std::vector<T> >& S, T x,T y)
@@ -220,24 +295,22 @@ void Graph<T>::merge(std::vector<std::vector<T> >& S, T x,T y)
 	//merge
 	for(int i = 0; i < tmp.size(); i++)
 		S.at(merge_index).push_back(tmp.at(i));
-
-	//for(int i = 0; i < S.at(merge_index).size(); i++)
-	//	std::cout << S.at(merge_index).at(i) << " ";
-	//std::cout << std::endl;
 }
 
+/*
+	segements the graph by putting each pixel in a 
+	component and merges pixels with similar 
+	intensity thus creating sub-regions of the graphs
+*/
 
 template <class T>
 void Graph<T>::Segment()
 {
-	/*for(auto i = pi.begin(); i != pi.end(); i++)
-		std::cout << i->first << " " << i->second.first << "," << i->second.second << std::endl;*/
-
 	//step 1
-	//sort E into pi, and sort non-decreasing order
+	//put E into pi, and sort pi non-decreasing order
 
 	//step 2
-	// each vertex is its own component
+	//put each vertex is in its own component
 	std::vector<std::vector<T> > S;
 	std::vector<T> tmp;
 	for(auto i = W.begin(); i != W.end(); i++)
@@ -248,15 +321,14 @@ void Graph<T>::Segment()
 	}
 
 	//step 3 & 4
+	//group by region
 	for(auto q = pi.begin(); q != pi.end(); q++)
 	{
-		if(isDisjoint(S,q->second.first,q->second.second) && q->first < internal_size(S,q->second.first) && q->first < internal_size(S,q->second.second))
-		{
+		//if disjoint and edge weight < internal component weight
+		if(isDisjoint(S,q->second.first,q->second.second) && q->first < internal_size(S,q->second.first,q->second.second))
 			merge(S,q->second.first,q->second.second);
-		}
 	}
 
-	//step 5
 	std::cout << "\n========================================\n";
 	std::cout << "\tSegmentation Result";
 	std::cout << "\n========================================\n";
@@ -266,7 +338,12 @@ void Graph<T>::Segment()
 		{
 			std::cout << "Region " << i+1 << ": ";
 			for(int j = 0; j < S.at(i).size(); j++)
-				std::cout << S.at(i).at(j) << " ";
+			{
+				std::cout << S.at(i).at(j) << "-";
+				auto iter = W.find(S.at(i).at(j));
+				std::cout << iter->second << "   ";
+
+			}
 			std::cout << "\n\n";
 		}
 	}
